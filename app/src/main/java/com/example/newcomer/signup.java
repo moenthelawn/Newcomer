19aa2b2df4dbd46e67caa634e1a6421aca882dc3
Chris Moen <chris.moen@bell.ca>
2019-12-18 23:12:04 -0500

    The first commit

---
+++ app/src/main/java/com/example/newcomer/signup.java
@@ -0,0 +1,13 @@
+package com.example.newcomer;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class signup extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+    }
+}
